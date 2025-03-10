d12e9e103c96fef85d56d8810d2d74fa5b41f5dd
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-03-10 16:35:14 +0900

    Improve this

--- lib/htmldiff.rb
+++ lib/htmldiff.rb
@@ -11,15 +11,15 @@ module HTMLDiff
 
   # Generate an HTML diff between two strings
   #
-  # @param a [String] the original string
-  # @param b [String] the new string
+  # @param old_string [String] the original string
+  # @param new_string [String] the new string
   # @option formatter [Object] an optional object which responds to `format`
   # @return [String] HTML diff of the two strings with additions and deletions marked
-  def diff(a, b, formatter: nil)
+  def diff(old_string, new_string, formatter: nil)
     formatter ||= OutputFormatter
-    a = Tokenizer.tokenize(a)
-    b = Tokenizer.tokenize(b)
-    operations = OperationGenerator.generate_operations(a, b)
-    formatter.format(operations, a, b)
+    old_tokens = Tokenizer.tokenize(old_string)
+    new_tokens = Tokenizer.tokenize(new_string)
+    operations = OperationGenerator.generate_operations(old_tokens, new_tokens)
+    formatter.format(operations, old_tokens, new_tokens)
   end
 end
