2b384ff2cfc76c869917b9ba5499dca8db4a306c
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-03-13 01:24:05 +0900

    Add :merge_threshold parameter

--- lib/html_diff.rb
+++ lib/html_diff.rb
@@ -16,18 +16,21 @@ module HTMLDiff
   #
   # @param old_string [String] The original string
   # @param new_string [String] The new string
-  # @option tokenizer [Object] An optional object which responds to `tokenize`,
+  # @option :tokenizer [Object] An optional object which responds to `tokenize`,
   #   which is used break the input strings into an Array of LCS-diffable tokens.
-  # @option format [Hash] An optional hash of options to pass to the formatter.
-  # @option formatter [Object] An optional object which responds to `format`,
+  # @option :format [Hash] An optional hash of options to pass to the formatter.
+  # @option :formatter [Object] An optional object which responds to `format`,
   #   which renders the LCS-diff output.
+  # @option :merge_threshold [Object] Maximum string length of unchanged tokens
+  #   to merge into neighboring changes. Value 0 merges only whitespace.
+  #   Negative values disable merging. Default value is 5.
   # @return [String] Diff of the two strings with additions and deletions marked.
-  def diff(old_string, new_string, tokenizer: nil, html_format: nil, formatter: nil)
+  def diff(old_string, new_string, tokenizer: nil, html_format: nil, formatter: nil, merge_threshold: nil)
     tokenizer ||= Tokenizer
     old_tokens = tokenizer.tokenize(old_string)
     new_tokens = tokenizer.tokenize(new_string)
 
-    changes = Differ.diff(old_tokens, new_tokens)
+    changes = Differ.diff(old_tokens, new_tokens, merge_threshold: merge_threshold)
 
     if html_format
       Formatters::HtmlFormatter.format(changes, **html_format)
--- lib/html_diff/differ.rb
+++ lib/html_diff/differ.rb
@@ -9,44 +9,58 @@ module HTMLDiff
     extend self
 
     # Constants for array indices
+    DEFAULT_MERGE_THRESHOLD = 5
     INDEX_ACTION = 0
     INDEX_OLD = 1
     INDEX_NEW = 2
-    INDEX_WS = 3
+    INDEX_MERGEABLE = 3
 
     # Generate a simplified diff from two sequences
     #
     # @param old_tokens [Array<String>] tokens from the original text
     # @param new_tokens [Array<String>] tokens from the new text
+    # @option :merge_threshold [Integer] Maximum string length of unchanged tokens
+    #   to merge into neighboring changes. Value 0 merges only whitespace.
+    #   Negative values disable merging. Default value is 5.
     # @return [Array<Array>] array of [action, old_string, new_string] tuples
-    def diff(old_tokens, new_tokens)
-      ops = build_ops(old_tokens, new_tokens)
+    def diff(old_tokens, new_tokens, merge_threshold: nil)
+      merge_threshold ||= DEFAULT_MERGE_THRESHOLD unless merge_threshold.is_a?(FalseClass)
+      ops = lcs_sdiff(old_tokens, new_tokens)
+
+      # First pass: compact consecutive operations of the same type
+      ops = compact_ops(ops)
+
+      # Second pass: merge consecutive unchanged operations
+      ops.each do |op|
+        op << mergeable_op?(op[INDEX_OLD], merge_threshold) if op[INDEX_ACTION] == '='
+      end
+
+      # Don't touch this code, it's optimized for performance.
       result = []
       i = 0
-
       while i < ops.length
         # Start a new group
-        curr = ops[i]
-        action = curr[INDEX_ACTION]
-        old_val = String.new(curr[INDEX_OLD] || '')
-        new_val = String.new(curr[INDEX_NEW] || '')
+        curr_op = ops[i]
+        action = curr_op[INDEX_ACTION]
+        old_val = String.new(curr_op[INDEX_OLD] || '')
+        new_val = String.new(curr_op[INDEX_NEW] || '')
         j = i + 1
 
-        # Check for patterns
-        while j < ops.length
+        # Check for patterns of the same operation type.
+        while j < ops.size
           next_op = ops[j]
 
-          if next_op[INDEX_ACTION] == action
+          if action == next_op[INDEX_ACTION]
             # Same operation type, append it
-            old_val << next_op[INDEX_OLD] if next_op[INDEX_OLD]
-            new_val << next_op[INDEX_NEW] if next_op[INDEX_NEW]
+            old_val << next_op[INDEX_OLD]
+            new_val << next_op[INDEX_NEW]
             j += 1
-          elsif next_op[INDEX_WS] && action == ops[j + 1]&.[](INDEX_ACTION)
-            # Whitespace followed by the same operation type, append both
-            old_val << next_op[INDEX_OLD] if next_op[INDEX_OLD]
-            old_val << ops[j + 1][INDEX_OLD] if ops[j + 1][INDEX_OLD]
-            new_val << next_op[INDEX_NEW] if next_op[INDEX_NEW]
-            new_val << ops[j + 1][INDEX_NEW] if ops[j + 1][INDEX_NEW]
+          elsif next_op[INDEX_MERGEABLE] && (next_next_op = ops[j + 1]) && (action == '!' || next_next_op[INDEX_ACTION] == '!')
+            # Mergeable segment preceded or followed by a replacement, combine everything
+            old_val << next_op[INDEX_OLD]
+            old_val << next_next_op[INDEX_OLD]
+            new_val << next_op[INDEX_NEW]
+            new_val << next_next_op[INDEX_NEW]
             j += 2
           else
             # Different pattern, break
@@ -66,32 +80,50 @@ module HTMLDiff
 
     private
 
+    def compact_ops(changes)
+      last = nil
+      last_action = nil
+      changes.each_with_object([]) do |change, result|
+        curr_action = change.action
+        curr_old = change.old_element
+        curr_new = change.new_element
+        if last_action == curr_action
+          last[INDEX_OLD] << curr_old if curr_old
+          last[INDEX_NEW] << curr_new if curr_new
+        elsif last_action && last_action != '=' && curr_action != '='
+          last[INDEX_ACTION] = '!'
+          last[INDEX_OLD] << curr_old if curr_old
+          last[INDEX_NEW] << curr_new if curr_new
+        else
+          result << (last = [curr_action, String.new(curr_old || ''), String.new(curr_new || '')])
+          last_action = curr_action
+        end
+      end
+    end
+
     # Perform LCS sdiff
     def lcs_sdiff(old_tokens, new_tokens)
       Diff::LCS.sdiff(old_tokens, new_tokens)
     end
 
-    # Convert to our format for easier processing
-    def build_ops(old_tokens, new_tokens)
-      lcs_sdiff(old_tokens, new_tokens).map do |diff|
-        if diff.action == '='
-          ['=', diff.old_element, diff.new_element, diff.old_element.strip.empty?]
-        else
-          ['!', diff.old_element || '', diff.new_element || '']
-        end
+    def mergeable_op?(str, merge_threshold)
+      if !merge_threshold || merge_threshold < 0
+        false
+      elsif merge_threshold == 0
+        str.strip.empty?
+      else
+        str.size <= merge_threshold || str.strip.empty?
       end
     end
 
     # Determine the final operation type
     def finalize_op(action, old_val, new_val)
-      if action == '!'
-        if old_val.empty?
-          action = '+'
-          old_val = nil
-        elsif new_val.empty?
-          action = '-'
-          new_val = nil
-        end
+      if old_val.empty?
+        action = '+'
+        old_val = nil
+      elsif new_val.empty?
+        action = '-'
+        new_val = nil
       end
 
       [action, old_val, new_val]
